1ee6b7360c9664d8515a782a01cbc9e1c0be1b8a
Francisco Lucas Janesch Lange Sens <franciscosensaulas@gmail.com>
2026-07-29 20:18:37 -0300

    Exemplos de try except com tratamento de erro

--- exemplo_10_try_except.py
+++ exemplo_10_try_except.py
@@ -25,7 +25,30 @@ def exemplo_com_tratamento_conversao():
     print("Acabou")
 
 
+def exemplo_com_multiplos_tratamentos():
+    numero1_digitado = "vinte e oito"
+    numero2_digitado = "nove"
+
+    try:
+        resultado: int = int(numero1_digitado) / int(numero2_digitado)
+        print("Resultado:", resultado)
+    except ZeroDivisionError:
+        print("Erro: Não é possível dividir por zero")
+    except ValueError:
+        print("Erro: os valores precisam ser números")
+
+    print("Obrigado por utilizar nosso sistema")
+
+def exemplo_mensagem_erro():
+    try:
+        aluno = {"nome": "Pedro", "nota1": 9.75}
+        media_aluno = aluno["media"]
+        print(media_aluno)
+    except KeyError as erro: # 'as' serve para pegar a variável do erro que ocorreu
+        print("Mensagem de erro tentar acessar a chave:", erro)
+
+
 # Ponto de entrada da aplicação, deve ter um único da aplicação inteira
 if __name__ == "__main__":
     # exemplo_sem_tratamento()
-    exemplo_com_tratamento_conversao()
+    exemplo_mensagem_erro()
